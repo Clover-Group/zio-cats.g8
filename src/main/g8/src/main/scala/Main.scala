0a94abc1eeabbe166404f743b872fa1044275ed3
tampler <socnetfpga@gmail.com>
2019-06-20 13:31:24 +0300

    Updated Cats Program

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -1,5 +1,19 @@
 package hello
 
+import scalaz.zio.{App, UIO, Task}
+import scala.collection.parallel.Task
+//import scalaz.zio.console.{putStrLn}
+
 object Main extends App {
-  println ("Hello")
+
+  def run(args: List[String]) =
+    res1.fold(_ => 1, _ => 0)
+  
+  // Succeed dummy
+  val res0 = UIO.succeed(0)  
+  
+  // Succeed with the value of program run 
+  val prog:List[List[Foo]] = Program[List].runme(List(Seed0, Seed1))  
+  val res1 = Task.succeed(prog)
+  
 }
---
+++ src/main/g8/src/main/scala/Program.scala
@@ -0,0 +1,23 @@
+package hello
+
+import cats.{Monad}
+import cats.implicits._
+
+sealed abstract class Foo // ADT
+final object Seed0 extends Foo
+final object Seed1 extends Foo
+
+// Custom Program
+sealed abstract class Program[F[_]:Monad] { self => 
+  def run[A](in:A):F[A]
+  def runme[A](in:A):List[A] = Program.ListFooProgram.run(in)
+}
+
+object Program {
+  def apply[F[_]](implicit F:Program[F]):Program[F] = F
+
+  implicit val ListFooProgram = new Program[List] {
+    def run[Foo](in:Foo) = List(in) 
+  }      
+
+}
